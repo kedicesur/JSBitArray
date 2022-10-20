15f7a0dbd9059bf4056d2f16bbaab0d119301565
Atriace <atriace@gmail.com>
2022-10-20 17:15:51 -0400

    Changed the constructor's two arguments into one. Added another error in the case of missing argument. Changed the length property to be a constant initialized during constructor Converted cases of Uint32 to Uint8.  This removed the necessity of the buffer to be sized in multiples of 32.  Arbitrarily large arrays can now be made.  Internally, a whole byte will be allocated, but the class will only operate up-to the desired size. Renamed "n" to "i" to clarify its intent as an "index" in the array Converted vars to "let" Added comments to a number of methods. Added a second argument to set(i, bool) in keeping with patterns found elsewhere (such as C#) Added setAll() which sets all values in the array to either 0 or 1 Updated toString() to support the arbitrarily long array lengths. Added clone() which copies the values from this BitArray to a new BitArray

--- BitArray.js
+++ BitArray.js
@@ -1,23 +1,35 @@
-class BitArray extends DataView{
-	constructor(n, ab) {
-		if (n > 1.5e10) throw new Error("BitArray size can not exceed 1.5e10");
-		super(ab instanceof ArrayBuffer ? ab : new ArrayBuffer(Number((BigInt(n + 31) & ~31n) >> 3n)));
-		// Sets ArrayBuffer.byteLength to multiples of 4 bytes (32 bits)
+export default class BitArray extends DataView {
+	constructor(sizeOrBuffer) {
+		let len = 0;
+		if (sizeOrBuffer instanceof ArrayBuffer) {
+			len = sizeOrBuffer.byteLength * 8;
+			super(sizeOrBuffer);
+		} else if (typeof sizeOrBuffer == "number") {
+			if (sizeOrBuffer > 1.5e10) { throw new Error("BitArray size can not exceed 1.5e10"); }
+			len = sizeOrBuffer;
+			super(new ArrayBuffer(Math.ceil(sizeOrBuffer/8)));
+		} else {
+			throw new Error("A size or buffer must be provided when initalizing a BitArray");
+		}
+
+		Object.defineProperty(this, "length", {
+			configurable: false,
+			enumerable: false,
+			writable: false,
+			value: len
+		});
 	}
 
-	get length() {
-		return this.buffer.byteLength << 3;
-	}
+	get popcount(){
+		let m1  = 0x55555555;
+		let m2  = 0x33333333;
+		let m4  = 0x0f0f0f0f;
+		let h01 = 0x01010101;
+		let pc  = 0;
+		let x;
 
-	get popcount() {
-		var m1  = 0x55555555,
-			m2  = 0x33333333,
-			m4  = 0x0f0f0f0f,
-			h01 = 0x01010101,
-			pc  = 0,
-			x;
-		for (var i = 0, len = this.buffer.byteLength >> 2; i < len; i++){
-			 x = this.getUint32(i << 2);
+		for (let i = 0; i < this.buffer.byteLength; i++){
+			 x = this.getUint8(i);
 			 x -= (x >> 1) & m1;             //put count of each 2 bits into those 2 bits
 			 x = (x & m2) + ((x >> 2) & m2); //put count of each 4 bits into those 4 bits
 			 x = (x + (x >> 4)) & m4;        //put count of each 8 bits into those 8 bits
@@ -26,58 +38,98 @@ class BitArray extends DataView{
 		return pc;
 	}
 
-	// n >> 3 is Math.floor(n/8)
-	// n & 7 is n % 8
-
-	and(bar) {
-		var len = Math.min(this.buffer.byteLength,bar.buffer.byteLength),
-			res = new BitArray(len << 3);
-		for (var i = 0; i < len; i += 4) res.setUint32(i,this.getUint32(i) & bar.getUint32(i));
-		return res;
+	at(i) {
+		// Fetches the value at the given index
+		if (i >= 0 && i < this.buffer.byteLength*8) {
+			return this.getUint8(i >> 3) & (1 << (i & 7)) ? 1 : 0;
+		}
+		return 0;
 	}
 
-	at(n) {  
-		return this.getUint8(n >> 3) & (1 << (n & 7)) ? 1 : 0;
-	}
-	
-	or(bar) {
-		var len = Math.min(this.buffer.byteLength,bar.buffer.byteLength),
-			res = new BitArray(len << 3);
-		for (var i = 0; i < len; i += 4) res.setUint32(i,this.getUint32(i) | bar.getUint32(i));
-		return res;
-	}
-	
-	not() {
-		var len = this.buffer.byteLength,
-			res = new BitArray(len << 3);
-		for (var i = 0; i < len; i += 4) res.setUint32(i,~(this.getUint32(i) >> 0));
-		return res;
+	set(i, bool = true) {
+		// Sets the value at the given index to the provided boolean
+		if (i >= 0 && i < this.buffer.byteLength*8) {
+			this.setUint8(i >> 3, this.getUint8(i >> 3) | (bool << (i & 7)));
+		}
 	}
 
-	reset(n) {
-		this.setUint8(n >> 3, this.getUint8(n >> 3) & ~(1 << (n & 7)));
+	reset(i) {
+		// Sets the value at the given index to 0
+		if (i >= 0 && i < this.buffer.byteLength*8) {
+			this.setUint8(i >> 3, this.getUint8(i >> 3) & ~(1 << (i & 7)));
+		}
 	}
 
-	set(n) {
-		this.setUint8(n >> 3, this.getUint8(n >> 3) | (1 << (n & 7)));
+	setAll(bool = false) {
+		// Sets all values to the provided boolean value (like zeroing the array);
+		for (let i = 0; i < this.buffer.byteLength; i++) {
+			this.setUint8(i, bool ? 255 : 0);
+		}
+	}
+
+	toggle(i) {
+		// Flips the value at the given index
+		if (i >= 0 && i < this.buffer.byteLength*8) {
+			this.setUint8(i >> 3, this.getUint8(i >> 3) ^ (1 << (i & 7)));
+		}
 	}
 
 	slice(a = 0, b = this.length) {
 		return new BitArray(b-a,this.buffer.slice(a >> 3, b >> 3));
 	}
 
-	toggle(n) {
-		this.setUint8(n >> 3, this.getUint8(n >> 3) ^ (1 << (n & 7)));
+	toString() {
+		let msg = [];
+		for (let i = 0; i < this.length; i++) {
+			msg.push(this.at(i));
+		}
+		return msg.join('');
 	}
 
-	toString() {
-		return new Uint8Array(this.buffer).reduce((p,c) => p + ((BigInt(c)* 0x0202020202n & 0x010884422010n) % 1023n).toString(2).padStart(8,"0"),"");
+	and(bar, newArray = false) {
+		// And of this and bar.  Example: 1100 & 1001 = 1000
+		let result = newArray ? new BitArray(this.length) : this;
+		for (let i = 0; i < this.buffer.byteLength; i++) {
+			result.setUint8(i, this.getUint8(i) & (i < bar.buffer.byteLength ? bar.getUint8(i) : 0));
+		}
+		return result;
 	}
 	
-	xor(bar) {
-		var len = Math.min(this.buffer.byteLength,bar.buffer.byteLength),
-			res = new BitArray(len << 3);
-		for (var i = 0; i < len; i += 4) res.setUint32(i,this.getUint32(i) ^ bar.getUint32(i));
-		return res;
+	or(bar, newArray = false) {
+		// Or of this and bar.  Example: 1100 & 1001 = 1101
+		let len = Math.min(this.buffer.byteLength, bar.buffer.byteLength);
+		let result = newArray ? new BitArray(this.length) : this;
+		for (let i = 0; i < len; i++) {
+			result.setUint8(i, this.getUint8(i) | bar.getUint8(i));
+		}
+		return result;
+	}
+
+	xor(bar, newArray = false) {
+		// Xor of this and bar.  Example: 1100 & 1001 = 0101;
+		let len = Math.min(this.buffer.byteLength, bar.buffer.byteLength);
+		let result = new BitArray(this.length);
+		for (let i = 0; i < len; i++) {
+			result.setUint8(i, this.getUint8(i) ^ bar.getUint8(i));
+		}
+		return result;
+	}
+	
+	not(newArray = false) {
+		// Flips all the bits in this buffer.  Example: 1100 = 0011
+		let result = newArray ? new BitArray(this.length) : this;
+		for (let i = 0; i < this.buffer.byteLength; i++) {
+			result.setUint8(i, ~(this.getUint8(i) >> 0));
+		}
+		return result;
+	}
+
+	clone() {
+		// Copies the values from this BitArray into a new BitArray
+		let result = new BitArray(this.length);
+		for (let i = 0; i < this.buffer.byteLength; i++) {
+			result.setUint8(i, this.getUint8(i));
+		}
+		return result;
 	}
 }
