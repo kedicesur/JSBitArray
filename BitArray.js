e8e35b1d1d01e3fabe9221c91ebcc0ede5f20ce3
Atriace <atriace@gmail.com>
2022-10-20 17:01:30 -0400

    Removed the private properties in favor of function scoped vars. Added support for BitArrays being passed to the constructor. Changed length() to buffer.byteLength << 3 Added popcount() which returns a tally of all 1s on the buffer. Added operators and(), or(), not(), & xor() Added slice() to generate a new array from the given indexes. Changed at(), and toggle() to use a simpler access pattern. Renamed put() to set(), and added reset() Updated toString() with 32-bit accesses using BigInt and padStart for formatting.

--- BitArray.js
+++ BitArray.js
@@ -1,52 +1,83 @@
-class BitArray extends DataView {	
-	#length = 0;
-	#mask   = new Uint8Array([
-		 0b10000000,
-		 0b01000000,
-		 0b00100000,
-		 0b00010000,
-		 0b00001000,
-		 0b00000100,
-		 0b00000010,
-		 0b00000001
-	]);
-	#mask_  = this.#mask.map(b => ~b);
-	#index  = 0;
-	#offset = 0;
-	
-	constructor(n) {
-		super(new ArrayBuffer(Math.ceil(n/8)));
-		this.#length = n;
+class BitArray extends DataView{
+	constructor(n, ab) {
+		if (n > 1.5e10) throw new Error("BitArray size can not exceed 1.5e10");
+		super(ab instanceof ArrayBuffer ? ab : new ArrayBuffer(Number((BigInt(n + 31) & ~31n) >> 3n)));
+		// Sets ArrayBuffer.byteLength to multiples of 4 bytes (32 bits)
 	}
-	
+
 	get length() {
-		return this.#length;
+		return this.buffer.byteLength << 3;
+	}
+
+	get popcount() {
+		var m1  = 0x55555555,
+			m2  = 0x33333333,
+			m4  = 0x0f0f0f0f,
+			h01 = 0x01010101,
+			pc  = 0,
+			x;
+		for (var i = 0, len = this.buffer.byteLength >> 2; i < len; i++){
+			 x = this.getUint32(i << 2);
+			 x -= (x >> 1) & m1;             //put count of each 2 bits into those 2 bits
+			 x = (x & m2) + ((x >> 2) & m2); //put count of each 4 bits into those 4 bits
+			 x = (x + (x >> 4)) & m4;        //put count of each 8 bits into those 8 bits
+			 pc += (x * h01) >> 56;
+		}
+		return pc;
+	}
+
+	// n >> 3 is Math.floor(n/8)
+	// n & 7 is n % 8
+
+	and(bar) {
+		var len = Math.min(this.buffer.byteLength,bar.buffer.byteLength),
+			res = new BitArray(len << 3);
+		for (var i = 0; i < len; i += 4) res.setUint32(i,this.getUint32(i) & bar.getUint32(i));
+		return res;
+	}
+
+	at(n) {  
+		return this.getUint8(n >> 3) & (1 << (n & 7)) ? 1 : 0;
 	}
 	
-	at(n) {
-		// Returns the bit at the given index.
-		this.#index = ~~(n/8);
-		this.#offset = n % 8;
-		return (this.getUint8(this.#index) & this.#mask[this.#offset]) >> (7-this.#offset);
+	or(bar) {
+		var len = Math.min(this.buffer.byteLength,bar.buffer.byteLength),
+			res = new BitArray(len << 3);
+		for (var i = 0; i < len; i += 4) res.setUint32(i,this.getUint32(i) | bar.getUint32(i));
+		return res;
 	}
 	
-	put(n, b) {
-		// Sets the value at the given index.
-		n < this.#length && (
-			this.#index = ~~(n/8),
-			this.#offset = n % 8,
-			this.setUint8(this.#index, b ? this.getUint8(this.#index) | this.#mask[this.#offset] : this.getUint8(this.#index) & this.#mask_[this.#offset])
-		);
+	not() {
+		var len = this.buffer.byteLength,
+			res = new BitArray(len << 3);
+		for (var i = 0; i < len; i += 4) res.setUint32(i,~(this.getUint32(i) >> 0));
+		return res;
+	}
+
+	reset(n) {
+		this.setUint8(n >> 3, this.getUint8(n >> 3) & ~(1 << (n & 7)));
+	}
+
+	set(n) {
+		this.setUint8(n >> 3, this.getUint8(n >> 3) | (1 << (n & 7)));
+	}
+
+	slice(a = 0, b = this.length) {
+		return new BitArray(b-a,this.buffer.slice(a >> 3, b >> 3));
 	}
 
 	toggle(n) {
-		// Flips the bit at the given index
-		this.#index = ~~(n/8);
-		this.#offset = n % 8;
-		this.setUint8(this.#index, this.getUint8(this.#index) ^ this.#mask[this.#offset]);
+		this.setUint8(n >> 3, this.getUint8(n >> 3) ^ (1 << (n & 7)));
+	}
+
+	toString() {
+		return new Uint8Array(this.buffer).reduce((p,c) => p + ((BigInt(c)* 0x0202020202n & 0x010884422010n) % 1023n).toString(2).padStart(8,"0"),"");
 	}
 	
-	toString() {
-		return new Uint8Array(this.buffer).reduce((p,c) => p.toString(2).padStart(8,"0")+c.toString(2).padStart(8,"0")).slice(0,this.#length);
+	xor(bar) {
+		var len = Math.min(this.buffer.byteLength,bar.buffer.byteLength),
+			res = new BitArray(len << 3);
+		for (var i = 0; i < len; i += 4) res.setUint32(i,this.getUint32(i) ^ bar.getUint32(i));
+		return res;
 	}
 }
