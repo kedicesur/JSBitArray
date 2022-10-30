8b73349d7a189a2b6f2d7a172b6364bd06314c73
Ömer Kaşdarma <omer.kasdarma@gmail.com>
2022-10-30 20:01:14 +0300

    Types added and constructor rephrased

---
+++ lib/BitArray.ts
@@ -0,0 +1,155 @@
+export default class BitArray extends DataView {
+
+  constructor(sizeOrBuffer : number | ArrayBuffer) {
+
+  interface BitArray extends DataView { length : number
+                                      , popcnt : number
+                                      }
+
+    let err : (e : string) => void = e => {throw new Error(e);},
+        arr : Uint8Array,
+        buf : ArrayBuffer;
+
+    /* If sizeOrBuffer is an ArrayBuffer with length which is not a multiple of 4 bytes then create a new TypedArray
+       with correct ArrayBuffer.length which is > sizeOrBuffer.byteLength. Fill the obtained TypedArray.buffer with 
+       supplied buffer starting from 0 and return a copy of the obtained ArrayBuffer which is divisible by 4 in length.
+       By returning a sliced copy of arr.buffer, arr is not kept under closure and eligible for GC.
+
+       If sizeOrBuffer is a number then prevent it from overshooting a reasonable size and create an ArrayBuffer with a
+       minimum byteLength which is a multiples of 4 bytes (32 bits) and > sizeOrBuffer.
+
+       If SizeOrBuffer is neither positive integer nor an ArrayBuffer then throw an error and return a 0 size ArrayBuffer
+       just to make sure that buf is of ArrayBuffer type. It will not get execuleted anyways.
+    */
+
+    buf = sizeOrBuffer instanceof ArrayBuffer ? sizeOrBuffer.byteLength % 4 ? ( arr = new Uint8Array(new ArrayBuffer((sizeOrBuffer.byteLength + 3) & ~3))
+		                                                                          , arr.set(new Uint8Array(sizeOrBuffer))
+																		                                          , arr.buffer.slice(0)
+	                                                                            )
+                                                                            : sizeOrBuffer                                       :
+          Number.isInteger(sizeOrBuffer)      ? ( sizeOrBuffer > 0x03ff000000 && err("BitArray size can not exceed 17163091968")
+		                                            , new ArrayBuffer(Number((BigInt(sizeOrBuffer + 31) & ~31n) >> 3n))
+	                                              )                                                                                :
+          /* otherwise */                       ( err("An integer size or an ArrayBUffer must be provided when initalizing a BitArray") 
+                                                , new ArrayBuffer(0) 
+                                                );
+    super(buf);
+    Object.defineProperty( this
+                         , "length"
+                         , { configurable: false
+                           , enumerable  : false
+                           , writable    : false
+                           , value       : this.buffer.byteLength * 8
+  	                       }
+                         );
+  }
+
+	get popcnt(){
+		let m1  = 0x55555555,
+		    m2  = 0x33333333,
+		    m4  = 0x0f0f0f0f,
+		    h01 = 0x01010101,
+		    pc  = 0,
+		    x;
+
+		for (let i = 0; i < this.buffer.byteLength; i += 4){
+			 x = this.getUint32(i);
+			 x -= (x >> 1) & m1;             //put count of each 2 bits into those 2 bits
+			 x = (x & m2) + ((x >> 2) & m2); //put count of each 4 bits into those 4 bits
+			 x = (x + (x >> 4)) & m4;        //put count of each 8 bits into those 8 bits
+			 pc += (x * h01) >> 24;
+		}
+		return pc;
+	}
+
+  all(){
+  // Returns true if all bits in the BitArray are set.
+    let len = this.buffer.byteLength,
+        res = true;
+    for (var i = 0; res && i < len; i += 4) res = this.getUint32(i) === 0xffffffff;
+    return res;
+  }
+
+  and(bar : BitArray, inPlace = false){
+  // And of this and bar. Example: 1100 & 1001 = 1000
+    let len = Math.min(this.buffer.byteLength,bar.buffer.byteLength),
+        res = inPlace ? this : new BitArray(len * 8);
+    for (var i = 0; i < len; i += 4) res.setUint32(i,this.getUint32(i) & bar.getUint32(i));
+    return res;
+  }
+
+  any(){
+  // Returns true if any of the bits in the BitArray are set. If returns false then all bits are 0
+    let len = this.buffer.byteLength,
+        res = true;
+    for (var i = 0; res && i < len; i += 4) res = this.getUint32(i) === 0;
+    return !res;
+  }
+
+  at(i : number){
+  // Fetches the value at the given index
+    return this.getUint8(~~(i / 8)) & (128 >> (i & 7)) ? 1 : 0;
+  }
+
+  clear(){
+  // Resets the BitArray in place
+    for (let i = 0, len = this.buffer.byteLength; i < len; i += 4) this.setUint32(i,0);
+  }
+
+  fill(){
+  // Sets the BitArray in place
+    for (let i = 0, len = this.buffer.byteLength; i < len; i += 4) this.setUint32(i,0xffffffff);
+  }
+
+  not(inPlace = false){
+  // Flips all the bits in this buffer. Example: 1100 = 0011
+    let len = this.buffer.byteLength,
+    res = inPlace ? this : new BitArray(len * 8);
+    for (var i = 0; i < len; i += 4) res.setUint32(i,~(this.getUint32(i) >>> 0));
+    return res;
+  }
+
+  or(bar : BitArray, inPlace = false){
+  // Or of this and bar. Example: 1100 & 1001 = 1101
+    let len = Math.min(this.buffer.byteLength,bar.buffer.byteLength),
+    res = inPlace ? this : new BitArray(len * 8);
+    for (var i = 0; i < len; i += 4) res.setUint32(i,this.getUint32(i) | bar.getUint32(i));
+    return res;
+  }
+
+  reset(i : number){
+	// Resets the value at the given index.
+    this.setUint8(~~(i / 8), this.getUint8(~~(i / 8)) & ~(128 >> (i & 7)));
+  }
+
+  set(i : number){
+	// Sets the value at the given index.
+    this.setUint8(~~(i / 8), this.getUint8(~~(i / 8)) | (128 >> (i & 7)));
+  }
+
+  slice(a = 0, b = this.buffer.byteLength){
+  // Slices BitArray and returns a new BitArray with buffer byteLength in multiples of 4 bytes (32 bits)
+  // The default argument values instantiate a clone.
+    b = a + Number((BigInt(b - a + 31) & ~31n));
+    return new BitArray(this.buffer.slice(a, b));
+  }
+
+  toggle(i : number){
+	// Flips the value at the given index
+    this.setUint8(~~(i / 8), this.getUint8(~~(i / 8)) ^ (128 >> (i & 7)));
+  }
+
+  // For efficiency maps this.buffer to an Uint8Array and byte by byte reverses the rank of bits and stringifies by
+  // reducing. However stringifying a huge BitArray is meaningless. Perhaps limiting the string size to 128 is reasonable.
+  toString(){
+    return new Uint8Array(this.buffer).reduce((p,c) => p + c.toString(2).padStart(8,"0"),"");
+  }
+
+  xor(bar : BitArray, inPlace = false){
+	// Xor of this and bar. Example: 1100 & 1001 = 0101;
+    let len = Math.min(this.buffer.byteLength,bar.buffer.byteLength),
+    res = inPlace ? this : new BitArray(len * 8);
+    for (var i = 0; i < len; i += 4) res.setUint32(i,this.getUint32(i) ^ bar.getUint32(i));
+    return res;
+  }
+}
