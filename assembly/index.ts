8e437693d9df5b3a32b58404b2a95830654c1395
Ömer Kaşdarma <omer.kasdarma@gmail.com>
2022-11-18 21:50:54 +0300

    modified:   assembly/index.ts Set max BitArray size to the AS GC friendly limit modified:   index.html

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,51 +1,25 @@
 // The entry file of your WebAssembly module.
+import { OBJECT_MAXSIZE } from "../node_modules/assemblyscript/std/assembly/rt/common";
 
 class BitArray extends DataView {
-  readonly length : u32;
-  readonly size   : u32;
-  [key : string]  : boolean;
+  readonly length : u64;
+  readonly size   : u64;
 
-  constructor(size : u32, buffer : ArrayBuffer ) {
-    let err : (e : string) => void = e => {throw new Error(e);},
-        arr : Uint8Array,
-        buf : ArrayBuffer;
-
-    /* If 'buffer' with length which is not a multiple of 4 bytes then create a new TypedArray with correct ArrayBuffer.
-       length which is > sizeOrBuffer.byteLength. Fill the obtained TypedArray.buffer with supplied buffer starting
-       from 0 and return a copy of the obtained ArrayBuffer which is divisible by 4 in length. By returning a sliced
-       copy of arr.buffer, arr is not kept under closure and "should be" eligible for GC.
-
-       If 'size' is a number then keep it within maximum indexable size and create an ArrayBuffer with a minimum
-       byteLength which is a multiples of 4 bytes (32 bits) and > sizeOrBuffer.
-
-       If 'size' is not a positive integer and 'buffer' is not an ArrayBuffer then throw an error.
-    */
-
-    buf = buffer instanceof ArrayBuffer ? ( size = buffer.byteLength * 8
-                                          , buffer.byteLength % 4 ? ( arr = Uint8Array.wrap(buffer,0,(buffer.byteLength + 3) & ~3)
-                                                                    , arr.buffer.slice(0)
-                                                                    )
-                                                                  : buffer
-                                          )                                                                                       :
-          Number.isInteger(size)        ? ( size > 0x07ffffffe0 && err("BitArray size can not exceed 34359738336")
-                                          , new ArrayBuffer(((size + 31) & ~(<u64>31)) >> 3)
-                                          )                                                                                       :
-          /* otherwise */                 err("An integer size or an ArrayBuffer must be provided when initalizing a BitArray");
-    
-    super(buf);
-    this.length = this.buffer.byteLength * 8;
+  constructor(size : u64, ab : ArrayBuffer) {
+    super(ab);
     this.size   = size;
+    this.length = this.buffer.byteLength * 8;
   }
 
-  get popcnt() : u32 {
-    let cnt = 0,
+  get popcnt() : u64 {
+    let cnt : u64 = 0,
         idx = 0,
         len = this.buffer.byteLength,
         mod = len % 8;
         
     if (mod && mod !== 4) throw new TypeError("Invalid BitArray.buffer.byteLength");
     len -= mod;
-    if (len) for (; idx < len; idx += 8) cnt += <u32>popcnt(this.getUint64(idx));
+    if (len) for (; idx < len; idx += 8) cnt += popcnt(this.getUint64(idx));
     if (mod) cnt += popcnt(this.getUint32(idx));
     return cnt;
   }
@@ -150,7 +124,7 @@ class BitArray extends DataView {
     
     if (mod && mod !== 4) throw new TypeError("Invalid BitArray.buffer.byteLength");
     len -= mod;
-    if (len) for (; idx < len; idx += 8) this.setUint64(idx, <u64>(Math.random() * f64.MAX_VALUE));
+    if (len) for (; idx < len; idx += 8) this.setUint64(idx, (<u64>(Math.random() * u32.MAX_VALUE) << 32) | <u32>(Math.random() * u32.MAX_VALUE));
     if (mod) this.setUint32(idx, <u32>(Math.random() * u32.MAX_VALUE));
     return this;
   }
@@ -195,12 +169,12 @@ class BitArray extends DataView {
     if (mod && mod !== 4) throw new TypeError("Invalid BitArray.buffer.byteLength");
     len -= mod;  
     if (len) for (; idx < len; idx += 8) str += this.getUint64(idx).toString(2).padStart(64,"0");
-    if (mod) str += this.getUint32(idx).toString(2).padStart(64,"0");
+    if (mod) str += this.getUint32(idx).toString(2).padStart(32,"0");
     return str;
   }
 
   wipe(b : boolean) : BitArray {
-  // Sets the BitArray in place
+  // Sets or resets the BitArray in place depending on the boolean value
     let idx = 0,
         len = this.buffer.byteLength,
         mod = len % 8,
@@ -236,8 +210,20 @@ class BitArray extends DataView {
 */
 }
 
-export function new_BitArray(n : u32, ab: ArrayBuffer) : BitArray {
-  return new BitArray(n,ab);
+export function from_ArrayBuffer(ab : ArrayBuffer) : BitArray {
+  const xs = (ab.byteLength + 3) & 0xfffffffc,
+        sz : u64 = ab.byteLength * 8;
+  if (<usize>ab.byteLength > OBJECT_MAXSIZE - 4) throw new RangeError(`Maximum allowed ArrayBuffer size is ${OBJECT_MAXSIZE - 4}`);
+  if (xs > ab.byteLength) ab = Uint8Array.wrap(ab,0,xs).buffer;
+  return new BitArray(sz,ab);
+}
+
+export function new_BitArray(n : u64) : BitArray {
+  const limit : u64      = <u64>OBJECT_MAXSIZE * 8 - 32,
+        byteLength : u32 = <u32>(((<u64>n + 31) & <u64>0x1ffffffe0) >> 3);
+
+  if (n > limit ) throw new RangeError(`Maximum allowed BitArray size is ${limit}`);
+  return new BitArray(<u64>n, new ArrayBuffer(byteLength)); // new ArrayBuffer(<u32>Math.ceil(<f32>n/32)*4)
 }
 
 export function all(target : BitArray) : boolean {
@@ -268,7 +254,7 @@ export function or(target : BitArray, source : BitArray, inPlace : boolean) : Bi
   return target.or(source,inPlace);
 }
 
-export function popcount(target : BitArray) : u32 {
+export function popcount(target : BitArray) : u64 {
   return target.popcnt;
 }
 
